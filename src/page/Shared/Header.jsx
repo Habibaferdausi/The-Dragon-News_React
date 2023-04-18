8e393c341ee620f1cf94d1496357ee27412df811
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-18 12:10:35 +0600

    add nav

--- src/page/Shared/Header.jsx
+++ src/page/Shared/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import moment from "moment";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
 const Header = () => {
   return (
@@ -18,8 +18,28 @@ const Header = () => {
           I can be react
         </Marquee>
       </div>
+
+      <Container>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mx-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#carrier">Carrier</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">Profile</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  <Button variant="secondary">Login</Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
     </Container>
   );
 };
-
 export default Header;
