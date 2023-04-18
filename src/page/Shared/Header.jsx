d28f345e4fbb47051f0c0d60f6b36f8950a55e2a
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-18 11:56:06 +0600

    add header

--- src/page/Shared/Header.jsx
+++ src/page/Shared/Header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-
+import moment from "moment";
+import { Button, Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 const Header = () => {
   return (
-    <div>
-      <img src="../../../logo.png" alt="" />
-    </div>
+    <Container>
+      <div className="text-center">
+        <img src="../../../logo.png" alt="" />
+        <p className="text-secondary mt-2">
+          <small>Journalism without Fear or Favour</small>
+        </p>
+        <p className="text-secondary">{moment().format("LLLL")}</p>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-danger" speed={50}>
+          I can be react
+        </Marquee>
+      </div>
+    </Container>
   );
 };
 
